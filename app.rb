7b8066a2bb1cfab1d4831e7465de1955ec3bc2cf
Samalzdh <sam.alzdh@gmail.com>
2023-10-17 15:21:42 +0200

    V2 correction tour par tour

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require_relative 'lib/player'
 player1 = Player.new("dipsy")
 player2 = Player.new("Po")
 
-puts "voici l'etat de chaque joueur : "
+puts "Bienvenu a l'examen des Chunins : "
 puts player1.show_state
 puts player2.show_state
 i = 1
@@ -15,19 +15,24 @@ while player1.life_points >0 && player2.life_points >0
   
   puts "Passons à la phase d'attaque :"
   puts 
-  puts "round #{i}"
+  puts "ROUND #{i}"
 
   player1.attacks(player2)
-  if player2.life_points == 0
-    puts player2.show_state
-    puts "victoire de #{player2.name}"
+  if player2.life_points <= 0 
+    
+    puts "victoire de #{player1.name}"
     puts 
     puts
   break
+
   else
     puts
     player2.attacks(player1)
-    
+    if player1.life_points <= 0 
+      
+      puts "victoire de #{player2.name}"
+      break
+    end
     puts 
     puts "--------------------------------------------------------"
   end
